1578ee85baacb87b1cc38d650392b6e2b006503f
skiwithuge <o.scicolone@gmail.com>
2018-03-27 23:07:43 +0200

    intent on map fragment (WIP) and arret pole d'echanges

--- app/src/main/java/com/skiwithuge/envimove/fragment/MapFragment.java
+++ app/src/main/java/com/skiwithuge/envimove/fragment/MapFragment.java
@@ -1,29 +1,36 @@
 package com.skiwithuge.envimove.fragment;
 
+import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.skiwithuge.envimove.Util.Locator;
+import com.skiwithuge.envimove.activity.BusStopActivity;
 import com.skiwithuge.envimove.model.BusStopList;
 import com.skiwithuge.envimove.MyApplication;
 import com.skiwithuge.envimove.R;
+
+import static com.skiwithuge.envimove.activity.MainActivity.ENVID;
 
 /**
  * Created by skiwi on 2/7/18.
  * Thanks to https://code.luasoftware.com/tutorials/android/supportmapfragment-in-fragment/
  */
 
-public class MapFragment extends Fragment implements OnMapReadyCallback {
+public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
+    private BusStopList list;
     private SupportMapFragment mapFragment;
     private GoogleMap mMap;
 
@@ -46,13 +53,26 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        BusStopList list = MyApplication.getBusStopList();
+        list = MyApplication.getBusStopList();
         mMap = googleMap;
         Locator lm = new Locator(this.getContext());
         lm.getLocation(Locator.Method.NETWORK_THEN_GPS, new MapFragment.MapLocator());
 
         for(BusStopList.BusStop b: list.mList){
             mMap.addMarker(new MarkerOptions().position(b.marker).title(b.name));
+        }
+
+        mMap.setOnInfoWindowClickListener(this);
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        for(BusStopList.BusStop b: list.mList) {
+            if(b.marker.equals(marker.getPosition())){
+                Intent myIntent = new Intent(getActivity(), BusStopActivity.class);
+                myIntent.putExtra(ENVID, b.env_id);
+                this.startActivity(myIntent);
+            }
         }
     }
 
