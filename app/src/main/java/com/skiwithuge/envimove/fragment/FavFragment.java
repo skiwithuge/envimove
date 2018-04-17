c82562159daa49164e3fe2d4b0bff95a5de894f2
skiwithuge <o.scicolone@gmail.com>
2018-04-17 21:59:51 +0200

    fav fragment

--- app/src/main/java/com/skiwithuge/envimove/fragment/FavFragment.java
+++ app/src/main/java/com/skiwithuge/envimove/fragment/FavFragment.java
@@ -1,10 +1,91 @@
 package com.skiwithuge.envimove.fragment;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.skiwithuge.envimove.MyApplication;
+import com.skiwithuge.envimove.R;
+import com.skiwithuge.envimove.Util.Locator;
+import com.skiwithuge.envimove.Util.SharedPreference;
+import com.skiwithuge.envimove.adapter.BusStopAdapter;
+import com.skiwithuge.envimove.interfaces.OnBusStopClickListener;
+import com.skiwithuge.envimove.interfaces.OnItemClickListener;
+import com.skiwithuge.envimove.model.BusStopList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by skiwi on 2/7/18.
  */
 
-public class FavFragment extends Fragment {
+public class FavFragment extends Fragment  implements OnItemClickListener<BusStopList.BusStop> {
+    BusStopList favList;
+    @BindView(R.id.day_recycler_view) RecyclerView mBusStopRecyclerView;
+    SharedPreference sharedPreference;
+    private BusStopAdapter mAdapter;
+    private OnBusStopClickListener mOnBusStopClickListener;
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mOnBusStopClickListener = (OnBusStopClickListener) context;
+        favList = new BusStopList();
+        // Get favorite items from SharedPreferences.
+        sharedPreference = new SharedPreference();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_fav, container, false);
+        ButterKnife.bind(this, v);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mBusStopRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        updateUI();
+
+    }
+
+    private void updateUI() {
+        favList.mList = sharedPreference.getFavorites(getActivity());
+
+        if (mAdapter == null) {
+            mAdapter = new BusStopAdapter(favList, this);
+            mBusStopRecyclerView.setAdapter(mAdapter);
+        } else {
+            mBusStopRecyclerView.setAdapter(mAdapter);
+            mAdapter.setBusStops(favList);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public void onItemClick(BusStopList.BusStop item) {
+        mOnBusStopClickListener.onBusStopSelected(item);
+    }
+
+    @Override
+    public void onFavClick(BusStopList.BusStop item) {
+        mOnBusStopClickListener.onFavSelected(item);
+        updateUI();
+    }
 }
