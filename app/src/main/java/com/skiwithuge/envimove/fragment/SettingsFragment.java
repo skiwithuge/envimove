addaf2f2d0d40af3a5007d45e4e0821e2cf3180b
skiwithuge <o.scicolone@gmail.com>
2018-06-29 22:51:24 +0200

    add settings/about fragment

--- app/src/main/java/com/skiwithuge/envimove/fragment/SettingsFragment.java
+++ app/src/main/java/com/skiwithuge/envimove/fragment/SettingsFragment.java
@@ -1,10 +1,25 @@
 package com.skiwithuge.envimove.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.skiwithuge.envimove.R;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by skiwi on 2/7/18.
  */
 
 public class SettingsFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
 }
